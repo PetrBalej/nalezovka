ce8182193d01bbc1ce5ad4daaed3943228c0732b
Petr Balej <petr.balej@nature.cz>
2021-01-12 15:52:15 +0100

    group_concat_max_len - zvětšeno kvůli omezení na Wbzdarma na 1024, což je nedostatečné...

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,21 +1,19 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class My_Controller extends CI_Controller {
-
-    public function __construct() {
-
+class My_Controller extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
 
         $map = directory_map(FCPATH . 'public', 1);
 
-        $this->public_sql = Array();
+        $this->public_sql = array();
 
         foreach ($map as $key => $value) {
-
             if (preg_match('/^dotaz([0-9]{2})\.sql$/', $value, $matches)) {
-
                 $this->public_sql[intval($matches[1])] = read_file(FCPATH . 'public/' . $value);
             }
         }
@@ -24,7 +22,6 @@ class My_Controller extends CI_Controller {
             show_error("Chybí soubory dotaz01.sql, dotaz02.sql atd. v adresáři public!", 404, "V adresáři public neexistují soubory s SQL dotazy!");
         }
 
-        
+        $this->db->query("SET group_concat_max_len = 1000000"); // na Webzdarma omezeno na 1024...
     }
-
 }
