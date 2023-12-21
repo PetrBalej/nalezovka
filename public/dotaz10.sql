e2e0abc85460571b9f79351073b711da99d9ec23
Petr Balej <54836928+PetrBalej@users.noreply.github.com>
2023-12-21 12:55:18 +0100

    hint funkce pro práci s polygonem

--- public/dotaz10.sql
+++ public/dotaz10.sql
@@ -4,4 +4,5 @@
 -- Nápověda: další tabulku (např. jezera) nemusíte nutně JOINovat, stačí ji přidat s čárkou: ... FROM tabulka1, tabulka2 ...
 -- V SELECTu musí být povinně použity tyto položky:
 -- gbifID, scientificName, ST_AsText(souradnice) AS souradniceWKT, souradnice, geo_poly
+-- Další prostorové funkce pro práci s polygony: https://mariadb.com/kb/en/polygon-properties/
 -- Zdůvodněte zde do poznámky, co je cílem Vámi navrhnutého SQL dotazu.
