82e1ac66f3cf1d0f2056608394e6fa74ba06d2de
Petr Balej <54836928+PetrBalej@users.noreply.github.com>
2024-01-09 17:40:35 +0100

    oprava mapových podkladů A

--- application/views/zaklad_konec_view.php
+++ application/views/zaklad_konec_view.php
@@ -180,11 +180,10 @@
     var mymap = L.map('mapid').setView(center, zoom);
 
     L.tileLayer(
-        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 18,
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
             id: 'mapbox/streets-v11'
         }).addTo(mymap);
 
