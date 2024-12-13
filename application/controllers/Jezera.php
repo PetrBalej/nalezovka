1b463260fcfa34bf07dae99daad9fca5827552a5
Petr Balej <petr@balej.net>
2024-12-13 08:20:15 +0100

    duplicite rows removed

--- application/controllers/Jezera.php
+++ application/controllers/Jezera.php
@@ -38,7 +38,6 @@ class Jezera extends MY_Controller
             $point = geoPHP::load($row['coordinatesWKT'], 'wkt');
             $geojson .= $point->out('json');
             $geojson .= ',"properties": {"speciesName": "<a href=\"' . site_url() . '/detail/index/' . $row['gbifID'] . '\" target=\"_blank\">' . $row['scientificName'] . ' (gbifID: ' . $row['gbifID'] . ')</a>"}},';
-            $geojson .= ',"properties": {"speciesName": "<a href=\"' . site_url() . '/detail/index/' . $row['gbifID'] . '\" target=\"_blank\">' . $row['scientificName'] . ' (gbifID: ' . $row['gbifID'] . ')</a>"}},';
         }
         $geojson .= ']}';
 
